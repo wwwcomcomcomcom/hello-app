8f6c68f8ef863f6768950d5475acf533be85d9a3
wwwcomcomcomcom <iieiiergn@gmail.com>
2025-01-16 21:05:23 +0900

    feat: get all users

--- src/main/java/com/example/domain/user/controller/UserController.java
+++ src/main/java/com/example/domain/user/controller/UserController.java
@@ -1,16 +1,24 @@
 package com.example.domain.user.controller;
 
 import com.example.domain.user.dto.CreateUserDto;
+import com.example.domain.user.dto.UserDto;
 import com.example.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    @GetMapping
+    public List<UserDto> getUsers() {
+        return userService.getUsers();
+    }
 
     @PostMapping("/join")
     public ResponseEntity<String> join(@RequestBody CreateUserDto user) {
