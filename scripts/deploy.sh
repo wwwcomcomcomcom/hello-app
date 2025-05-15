649e4ff373f3816eac7ea3c1ccea8483f871e838
wwwcomcomcomcom <iieiiergn@gmail.com>
2025-05-15 22:30:11 +0900

    hotfix: docker permission

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,16 +4,16 @@ IMAGE_NAME=hello-app-img
 CONTAINER_NAME=hello-app
 DOCKERFILE_NAME=Dockerfile
 
-CURRENT_CONTAINER_ID=$(docker ps -a -q -f name=$CONTAINER_NAME)
+CURRENT_CONTAINER_ID=$(sudo docker ps -a -q -f name=$CONTAINER_NAME)
 
 if [ ! -z "$CURRENT_CONTAINER_ID" ]
 then
   echo "현재 구동 중인 컨테이너가 있습니다. 중지하고 삭제합니다."
-  docker stop $CONTAINER_NAME || true
-  docker rm $CONTAINER_NAME
+  sudo docker stop $CONTAINER_NAME || true
+  sudo docker rm $CONTAINER_NAME
 fi
 
-cd /home/ubuntu
-docker build -t $IMAGE_NAME -f $DOCKERFILE_NAME .
+cd /home/ubuntu || exit
+sudo docker build -t $IMAGE_NAME -f $DOCKERFILE_NAME .
 
-docker run -d --name $CONTAINER_NAME -p 8080:8080 --restart always $IMAGE_NAME
+sudo docker run -d --name $CONTAINER_NAME -p 8080:8080 --restart always $IMAGE_NAME
