cd577a1097dfea90ba5ca4db0a0d4bd1c45feb21
wwwcomcomcomcom <iieiiergn@gmail.com>
2025-05-15 19:43:22 +0900

    feat: health check

---
+++ src/main/java/com/example/global/util/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.example.global.util;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+    @RequestMapping
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK");
+    }
+}
